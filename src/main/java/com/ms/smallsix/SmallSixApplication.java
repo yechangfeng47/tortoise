4697a4cbf0fdaed544888738b9ad04bf636797cb
yechangfeng47 <275820797@qq.com>
2019-06-04 23:44:23 +0800

    update

---
+++ src/main/java/com/ms/smallsix/SmallSixApplication.java
@@ -0,0 +1,13 @@
+package com.ms.smallsix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallSixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallSixApplication.class, args);
+	}
+
+}
